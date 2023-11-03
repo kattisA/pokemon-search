dbb080668ea4dd8a09bb76e4a2279846136c6442
Katarina Lejonlid <katarina.lejonlid@nbis.se>
2023-11-03 16:26:10 +0100

    Adding more information such as description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,52 @@ import React, {useState} from 'react';
 import SearchBar from "@/components/SearchBar";
 import Alert from "@/components/Alert"
 
+interface PokemonType {
+  slot: number;
+  type: {
+    name: string;
+    url: string;
+  };
+}
+
+interface PokemonAbility {
+  ability: {
+    name: string;
+    url: string;
+  };
+  is_hidden: boolean;
+  slot: number;
+}
+
+interface PokemonSpriteVersions {
+  'official-artwork': {
+    front_default: string;
+  };
+}
+
+interface PokemonSprites {
+  front_default: string;
+  other: PokemonSpriteVersions;
+}
+
+
+interface PokemonDetails {
+  name: string;
+  types: PokemonType[];
+  sprites: PokemonSprites;
+  abilities: PokemonAbility[];
+}
+
+interface PokemonState {
+  basic: PokemonDetails | null;
+  description: string;
+}
 
 export default function Home() {
-  const [pokemon, setPokemon] = useState(null);
+  const [pokemon, setPokemon] = useState<PokemonState>({
+    basic: null,
+    description:"",
+  });
   const [searchValue, setSearchValue] = useState("");
   const [errorMessage, setErrorMessage] = useState('');
 
@@ -26,23 +69,38 @@ export default function Home() {
 
     try {
       const url = `https://pokeapi.co/api/v2/pokemon/${searchValue.toLowerCase()}`;
-      const response = await fetch(url);
-      const responseJson = await response.json();
+      const speciesUrl = `https://pokeapi.co/api/v2/pokemon-species/${searchValue.toLowerCase()}`;
+      const [response, speciesResponse] = await Promise.all([
+        fetch(url),
+        fetch(speciesUrl)
+      ]);
 
-      if (responseJson) {
-        const dataToStore = {
-          data: responseJson,
-          timestamp: currentTime
-        };
-        localStorage.setItem(searchValue.toLowerCase(), JSON.stringify(dataToStore));
+      const [pokemonDetails, speciesDetails] = await Promise.all([
+        response.json(),
+        speciesResponse.json()
+      ]);
 
-        setPokemon(responseJson);
-      } else {
-        setPokemon(null);
-      }
+      const descriptionTextEntry = speciesDetails.flavor_text_entries.find(entry => entry.language.name === 'en');
+      const descriptionText = descriptionTextEntry ? descriptionTextEntry.flavor_text : "No description available.";
+
+      const newPokemonData = {
+        basic: pokemonDetails,
+        description: descriptionText
+      };
+
+      setPokemon(newPokemonData);
+
+      localStorage.setItem(searchValue, JSON.stringify({
+        timestamp: new Date().getTime(),
+        data: newPokemonData
+      }));
     } catch (error) {
       setErrorMessage("The Pokemon was not found. Try again!")
-      setPokemon(null);
+      console.error("Failed to fetch Pokémon data:", error);
+      setPokemon({
+        basic: null,
+        description: "",
+      });
     }
   };
 
@@ -68,17 +126,27 @@ export default function Home() {
                 <Alert message={errorMessage} title={"Error"}/>
             )}
 
-            {pokemon && (
+            {pokemon.basic && (
                 <div className="max-w-sm rounded overflow-hidden shadow-lg">
-                  <img className="w-full" src={pokemon.sprites.other["official-artwork"].front_default} alt={pokemon.name} />
+                  <img className="w-full" src={pokemon.basic.sprites.other["official-artwork"].front_default} alt={pokemon.basic.name} />
                   <div className="px-6 py-4">
-                    <div className="font-bold text-xl mb-2">{capitalizeFirstLetter(pokemon.name)} </div>
-                    <p className="text-gray-700 text-base">
-                      {pokemon.types.map(typeInfo => typeInfo.type.name).join(", ")}
-                    </p>
+                    <div className="font-bold text-xl mb-2">{capitalizeFirstLetter(pokemon.basic.name)} </div>
+                    <div className="px-6 pt-4 pb-2">
+                      {pokemon.basic.types.map(typeInfo => (
+                          <span key={typeInfo.type.name}
+                                className="inline-block bg-pokemon-purple rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">
+                              {typeInfo.type.name}
+                        </span>
+                      ))}
+                    </div>
+                    {pokemon.description && (
+                        <p className="text-gray-700 text-base">
+                          {pokemon.description.replace(/[\f]/g, ' ')}
+                        </p>
+                    )}
                   </div>
                   <div className="px-6 pt-4 pb-2">
-                    {pokemon.abilities.map(abilityInfo => (
+                    {pokemon.basic.abilities.map(abilityInfo => (
                         <span key={abilityInfo.ability.name}
                               className="inline-block bg-pokemon-purple rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">
                               {abilityInfo.ability.name}
