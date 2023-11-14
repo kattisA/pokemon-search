75079ebfe8817bb56d9974e8b8881cd22b931050
Katarina Lejonlid <katarina.lejonlid@nbis.se>
2023-11-14 16:55:27 +0100

    Added evolution to the app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,172 +4,214 @@ import SearchBar from "@/components/SearchBar";
 import Alert from "@/components/Alert"
 
 interface PokemonType {
-  slot: number;
-  type: {
-    name: string;
-    url: string;
-  };
+    slot: number;
+    type: {
+        name: string;
+        url: string;
+    };
 }
 
 interface PokemonAbility {
-  ability: {
-    name: string;
-    url: string;
-  };
-  is_hidden: boolean;
-  slot: number;
+    ability: {
+        name: string;
+        url: string;
+    };
+    is_hidden: boolean;
+    slot: number;
 }
 
 interface PokemonSpriteVersions {
-  'official-artwork': {
-    front_default: string;
-  };
+    'official-artwork': {
+        front_default: string;
+    };
 }
 
 interface PokemonSprites {
-  front_default: string;
-  other: PokemonSpriteVersions;
+    front_default: string;
+    other: PokemonSpriteVersions;
 }
 
 
 interface PokemonDetails {
-  name: string;
-  types: PokemonType[];
-  sprites: PokemonSprites;
-  abilities: PokemonAbility[];
+    name: string;
+    types: PokemonType[];
+    sprites: PokemonSprites;
+    abilities: PokemonAbility[];
 }
 
 interface PokemonState {
-  basic: PokemonDetails | null;
-  description: string;
+    basic: PokemonDetails | null;
+    description: string;
+    evolutionNames: string[];
+}
+
+interface EvolutionChainLink {
+    speciesName: string;
+    evolvesTo: EvolutionChainLink[]; // Recursive structure for multi-stage evolutions
+}
+
+interface EvolutionChainResponse {
+    chain: EvolutionChainLink;
 }
 
 export default function Home() {
-  const [pokemon, setPokemon] = useState<PokemonState>({
-    basic: null,
-    description:"",
-  });
-  const [searchValue, setSearchValue] = useState("");
-  const [errorMessage, setErrorMessage] = useState('');
-
-  const CACHE_DURATION = 86400000;
-
-  const fetchCharacter = async (searchValue) => {
-    const currentTime = new Date().getTime();
-    const storedData = localStorage.getItem(searchValue.toLowerCase());
-
-    if (storedData) {
-      const { data, timestamp } = JSON.parse(storedData);
-
-      if (currentTime - timestamp < CACHE_DURATION) {
-        setPokemon(data);
-        return;
-      }
-    }
-
-    try {
-      const url = `https://pokeapi.co/api/v2/pokemon/${searchValue.toLowerCase()}`;
-      const speciesUrl = `https://pokeapi.co/api/v2/pokemon-species/${searchValue.toLowerCase()}`;
-      const [response, speciesResponse] = await Promise.all([
-        fetch(url),
-        fetch(speciesUrl)
-      ]);
-
-      const [pokemonDetails, speciesDetails] = await Promise.all([
-        response.json(),
-        speciesResponse.json()
-      ]);
-
-      const descriptionTextEntry = speciesDetails.flavor_text_entries.find(entry => entry.language.name === 'en');
-      const descriptionText = descriptionTextEntry ? descriptionTextEntry.flavor_text : "No description available.";
-
-      const newPokemonData = {
-        basic: pokemonDetails,
-        description: descriptionText
-      };
-
-      setPokemon(newPokemonData);
-
-      localStorage.setItem(searchValue, JSON.stringify({
-        timestamp: new Date().getTime(),
-        data: newPokemonData
-      }));
-    } catch (error) {
-      setErrorMessage("The Pokemon was not found. Try again!")
-      console.error("Failed to fetch Pokémon data:", error);
-      setPokemon({
+    const [pokemon, setPokemon] = useState<PokemonState>({
         basic: null,
         description: "",
-      });
+        evolutionNames: []
+
+    });
+    const [searchValue, setSearchValue] = useState("");
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const CACHE_DURATION = 86400000;
+
+    const fetchCharacter = async (searchValue) => {
+        const currentTime = new Date().getTime();
+        const storedData = localStorage.getItem(searchValue.toLowerCase());
+
+        if (storedData) {
+            const {data, timestamp} = JSON.parse(storedData);
+
+            if (currentTime - timestamp < CACHE_DURATION) {
+                setPokemon(data);
+                return;
+            }
+        }
+
+        try {
+            const url = `https://pokeapi.co/api/v2/pokemon/${searchValue.toLowerCase()}`;
+            const speciesUrl = `https://pokeapi.co/api/v2/pokemon-species/${searchValue.toLowerCase()}`;
+            const [response, speciesResponse] = await Promise.all([
+                fetch(url),
+                fetch(speciesUrl)
+            ]);
+
+            const [pokemonDetails, speciesDetails] = await Promise.all([
+                response.json(),
+                speciesResponse.json()
+            ]);
+
+            const descriptionTextEntry = speciesDetails.flavor_text_entries.find(entry => entry.language.name === 'en');
+            const descriptionText = descriptionTextEntry ? descriptionTextEntry.flavor_text : "No description available.";
+
+            // Fetch evolution chain
+            const evolutionResponse = await fetch(speciesDetails.evolution_chain.url);
+            const evolutionChainData = await evolutionResponse.json();
+
+            let evolutionNames = [];
+            let currentChainLink = evolutionChainData.chain;
+
+            while (currentChainLink && currentChainLink.species) {
+                evolutionNames.push(currentChainLink.species.name);
+                currentChainLink = currentChainLink.evolves_to[0]; // Assuming a single linear evolution path
+            }
+
+            const newPokemonData = {
+                basic: pokemonDetails,
+                description: descriptionText,
+                evolutionNames: evolutionNames
+            };
+
+            setPokemon(newPokemonData);
+
+            localStorage.setItem(searchValue, JSON.stringify({
+                timestamp: new Date().getTime(),
+                data: newPokemonData
+            }));
+        } catch (error) {
+            setErrorMessage("The Pokemon was not found. Try again!")
+            console.error("Failed to fetch Pokémon data:", error);
+            setPokemon({
+                basic: null,
+                description: "",
+                evolutionNames: []
+            });
+        }
+    };
+
+    const handleSearch = (event) => {
+        event.preventDefault();
+        setErrorMessage('');
+        fetchCharacter(searchValue);
+    };
+
+    const capitalizeFirstLetter = (string) => {
+        return string.charAt(0).toUpperCase() + string.slice(1);
     }
-  };
 
-  const handleSearch = (event) => {
-    event.preventDefault();
-    setErrorMessage('');
-    fetchCharacter(searchValue);
-  };
-
-  const capitalizeFirstLetter = (string) => {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-  }
-
-  return (
-      <main className="bg-pokemon-theme bg-no-repeat bg-cover bg-center relative flex min-h-screen flex-col items-center justify-center">
-        <h1 className="pt-4 pb-8 bg-white shadow-xl ring-1 ring-gray-900/5 rounded-lg backdrop-blur-lg max-w-xl mx-auto w-full text-center text-3xl font-medium tracking-tight text-pokemon-dark-purple ">
-          Pokemon Search App
-        </h1>
-        <div className="bg-white p-12 shadow-xl ring-1 ring-gray-900/5 rounded-lg backdrop-blur-lg max-w-xl mx-auto w-full min-h-[200px] flex items-center justify-center">
-          <div className="max-w-sm rounded overflow-hidden shadow-lg">
-           <SearchBar searchValue={searchValue} setSearchValue={setSearchValue} handleSearch={handleSearch}/>
-            {errorMessage && (
-                <Alert message={errorMessage} title={"Error"}/>
-            )}
-
-            {pokemon.basic && (
+    return (
+        <main
+            className="bg-pokemon-theme bg-no-repeat bg-cover bg-center relative flex min-h-screen flex-col items-center justify-center">
+            <h1 className="pt-4 pb-8 bg-white shadow-xl ring-1 ring-gray-900/5 rounded-lg backdrop-blur-lg max-w-xl mx-auto w-full text-center text-3xl font-medium tracking-tight text-pokemon-dark-purple ">
+                Pokemon Search App
+            </h1>
+            <div
+                className="bg-white p-12 shadow-xl ring-1 ring-gray-900/5 rounded-lg backdrop-blur-lg max-w-xl mx-auto w-full min-h-[200px] flex items-center justify-center">
                 <div className="max-w-sm rounded overflow-hidden shadow-lg">
-                  <img className="w-full" src={pokemon.basic.sprites.other["official-artwork"].front_default} alt={pokemon.basic.name} />
-                  <div className="px-6 py-4">
-                    <div className="font-bold text-xl mb-2">{capitalizeFirstLetter(pokemon.basic.name)} </div>
-                    <div className="px-2 pt-4 pb-2">
-                      {pokemon.basic.types.map(typeInfo => (
-                          <span key={typeInfo.type.name}
-                                className="inline-block bg-grey-blue rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">
+                    <SearchBar searchValue={searchValue} setSearchValue={setSearchValue} handleSearch={handleSearch}/>
+                    {errorMessage && (
+                        <Alert message={errorMessage} title={"Error"}/>
+                    )}
+
+                    {pokemon.basic && (
+                        <div className="max-w-sm rounded overflow-hidden shadow-lg">
+                            <img className="w-full" src={pokemon.basic.sprites.other["official-artwork"].front_default}
+                                 alt={pokemon.basic.name}/>
+                            <div className="px-6 py-4">
+                                <h2
+                                    className="font-bold text-xl mb-2">{capitalizeFirstLetter(pokemon.basic.name)} </h2>
+                                <div className="px-2 pt-4 pb-2">
+                                    {pokemon.basic.types.map(typeInfo => (
+                                        <span key={typeInfo.type.name}
+                                              className="inline-block bg-grey-blue rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">
                               {typeInfo.type.name}
                         </span>
-                      ))}
-                    </div>
-                    {pokemon.description && (
-                        <p className="text-gray-700 text-base">
-                          {pokemon.description.replace(/[\f]/g, ' ')}
-                        </p>
-                    )}
-                  </div>
-                  <div className="px-6 pt-4 pb-2">
-                    {pokemon.basic.abilities.map(abilityInfo => (
-                        <span key={abilityInfo.ability.name}
-                              className="inline-block bg-pokemon-purple rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">
+                                    ))}
+                                </div>
+                                {pokemon.description && (
+                                    <p className="text-gray-700 text-base">
+                                        {pokemon.description.replace(/[\f]/g, ' ')}
+                                    </p>
+                                )}
+                            </div>
+                            <div className="px-6 pt-4 pb-2">
+                                {pokemon.basic.abilities.map(abilityInfo => (
+                                    <span key={abilityInfo.ability.name}
+                                          className="inline-block bg-pokemon-purple rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">
                               {abilityInfo.ability.name}
                         </span>
-                    ))}
-                  </div>
+                                ))}
+                            </div>
+                            {pokemon.evolutionNames && pokemon.evolutionNames.length > 0 && (
+                                <div className="px-6 pt-4 pb-2 text-gray-700">
+                                    <h2 className="font-bold text-xl mb-2">Evolutions</h2>
+                                        {pokemon.evolutionNames.map((name, index) => (
+                                            !(pokemon.basic) || name === pokemon.basic.name ?
+                                           <span className="font-bold"> {capitalizeFirstLetter(name)}</span> :
+                                                <span> {capitalizeFirstLetter(name)}</span>
+                                        ))}
+                                </div>
+                            )
+                            }
+                        </div>
+                    )}
                 </div>
-            )}
-          </div>
-        </div>
-        <footer className="bg-white border-t mt-8 py-4">
-          <div className="max-w-xl mx-auto px-4 text-center">
-            <p className="text-sm text-gray-600">
-              Created by
-              {' '}
-              <a href="https://kattisa.github.io/" target="_blank" rel="noopener noreferrer" className="text-pokemon-dark-purple hover:text-pokemon-light-purple">
-                Kattis
-              </a>
-            </p>
-          </div>
-        </footer>
+            </div>
+            <footer className="bg-white border-t mt-8 py-4">
+                <div className="max-w-xl mx-auto px-4 text-center">
+                    <p className="text-sm text-gray-600">
+                        Created by
+                        {' '}
+                        <a href="https://kattisa.github.io/" target="_blank" rel="noopener noreferrer"
+                           className="text-pokemon-dark-purple hover:text-pokemon-light-purple">
+                            Kattis
+                        </a>
+                    </p>
+                </div>
+            </footer>
 
-      </main>
+        </main>
 
-  );
+    );
 }
