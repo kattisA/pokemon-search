e08b15ff27663b271eafd638d4f21f5afc078c02
Katarina Lejonlid <katarina.lejonlid@nbis.se>
2023-11-17 20:59:23 +0100

    export path map

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
     images: {
         domains: ['raw.githubusercontent.com'],
     },
+    exportPathMap: async function () {
+        return {
+            '/': { page: '/' },
+        };
+    },
 };
 
 module.exports = nextConfig;
